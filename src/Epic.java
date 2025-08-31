fd2a874c6ecc5ea0e6fe002962b7c3836a64902d
Inessa Babenko <iness.tt.eln@gmail.com>
2025-08-31 14:49:30 +0700

    add set status for epic

--- src/Epic.java
+++ src/Epic.java
@@ -2,15 +2,37 @@ import java.util.ArrayList;
 
 public class Epic extends Task{
     ArrayList<Subtask> subtasksList;
+    ArrayList<Integer> subtaskIds = new ArrayList<>();
 
-    Epic(String title, String description, Status status, ArrayList<Subtask> subtasksList) {
-        super(title, description, status);
+    Epic(String title, String description, ArrayList<Subtask> subtasksList) {
+        super(title, description);
         this.subtasksList = subtasksList;
+        setStatus(subtasksList);
+        for (Subtask subtask : subtasksList) {
+            subtaskIds.add(subtask.getId());
+            subtask.setEpicId(this.getId());
+        }
     }
 
-    @Override
-    public String toString () {
-        return this.title + " " + this.getId() + " "  + this.description + " "  + this.status;
-
+    private void setStatus (ArrayList<Subtask> subtasksList) {
+        int statusNew = 0;
+        int statusDone = 0;
+        for (Subtask subtask : subtasksList) {
+            if (subtask.getStatus().equals(Status.NEW)) {
+                statusNew++;
+            } else if (subtask.getStatus().equals(Status.DONE)) {
+                statusDone++;
+            }
+        }
+        if (subtasksList.size() == statusNew) {
+            this.status = Status.NEW;
+        } else if (subtasksList.size() == statusDone) {
+            this.status = Status.DONE;
+        } else {
+            this.status = Status.IN_PROGRESS;
+        }
     }
+
+
+
 }
