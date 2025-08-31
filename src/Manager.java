f1df676acca0df1442526f65ae68e5ba8c35a887
Inessa Babenko <iness.tt.eln@gmail.com>
2025-08-31 11:25:27 +0700

    add 1) get task by id 2) remove task by is 3) remove all tasks

--- src/Manager.java
+++ src/Manager.java
@@ -1,9 +1,36 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class Manager {
-    ArrayList<Task> taskList = new ArrayList<>();
-    ArrayList<Epic> epicList = new ArrayList<>();
+    HashMap<Integer, Object> taskList = new HashMap<>();
+
+    public void addTask(Object task) {
+        String taskClass = String.valueOf(task.getClass());
+        Integer id;
+        if (taskClass.equals("class Task")) {
+            Task newTask = (Task) task;
+            id = newTask.getId();
+        } else {
+            Epic newTask = (Epic) task;
+            id = newTask.getId();
+        }
+        taskList.put(id, task);
+    }
+
+    public Object getTaskById(int id) {
+        if (taskList.containsKey(id)) {
+            return taskList.get(id);
+        }
+        return "no task by id" + id;
+    }
+
+    public void removeTaskById(int id){
+       taskList.remove(id);
+    }
+    public void removeAll() {
+        taskList.clear();
+    }
 
 
 }
+
