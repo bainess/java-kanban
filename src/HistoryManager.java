44cb5809e84145cbe5f516043f867b7537a49fc7
Inessa Babenko <iness.tt.eln@gmail.com>
2025-09-14 20:34:38 +0700

    add HistoryManager method add and getHistory

--- src/HistoryManager.java
+++ src/HistoryManager.java
@@ -1,22 +1,7 @@
 import java.util.ArrayList;
-import java.util.List;
 
-public class HistoryManager {
-    ArrayList<Task> historyList = new ArrayList<>();
+public interface HistoryManager {
+    void add(Task task);
 
-    public void add(Task task) {
-        if (historyList.size() < 11) {
-            historyList.add(task);
-        } else {
-            ArrayList<Task> historyListTemp = new ArrayList<>();
-            for (int i = 1; i < historyList.size(); i++) {
-                historyListTemp.add(historyList.get(i));
-            }
-            historyListTemp.add(task);
-            historyList = historyListTemp;
-        }
-    }
-    public ArrayList<Task> getHistory() {
-        return historyList;
-    }
+    ArrayList<Task> getHistory();
 }
