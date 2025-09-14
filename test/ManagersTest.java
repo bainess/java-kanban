78ce49d82819aeb1db3082a5351717083cbba5c0
Inessa Babenko <iness.tt.eln@gmail.com>
2025-09-14 20:33:50 +0700

    add ManagersTest

---
+++ test/ManagersTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManagersTest {
+    @Test
+    void shouldBeNotNullWhenCallGetDefault(){
+        Managers managers = new Managers();
+        assertNotNull(managers);
+    }
+}
