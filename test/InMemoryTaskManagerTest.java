30cb930030de4365be2e1e43e854d008e900a01d
Inessa Babenko <iness.tt.eln@gmail.com>
2025-09-15 16:14:39 +0700

    remove unsused import statements

--- test/InMemoryTaskManagerTest.java
+++ test/InMemoryTaskManagerTest.java
@@ -1,8 +1,6 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.AfterEach;
 class InMemoryTaskManagerTest {
     static InMemoryTaskManager taskManager;
     @BeforeAll
