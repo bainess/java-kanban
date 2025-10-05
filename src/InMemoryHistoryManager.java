8b18ce08c5c2f00730863f7c107147c1d955b0b8
Inessa Babenko <iness.tt.eln@gmail.com>
2025-10-06 00:18:19 +0700

    feat: remove repeated Task in HistoryList

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -1,69 +1,74 @@
 import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    HistoryLinkedList<Task> historyList = new HistoryLinkedList<>();
-    Map<Integer, Task> historyListIdHolder= new HashMap<>();
+    private Node<Task> start;
+    private Node<Task> end;
+    int size = 0;
+    Map<Integer, Node<Task>> historyListIdHolder= new HashMap<>();
 
     @Override
     public void addToHistoryList(Task task) {
-        if (historyList.getSize() > 10) {
+        boolean flag = historyListIdHolder.containsKey(task.getId());
+        if (flag) {
+            removeNode(historyListIdHolder.get(task.getId()));
             historyListIdHolder.remove(task.getId());
-            historyList.removeFirst();
-            historyList.addLast(task);
-            historyListIdHolder.put(task.getId(), task);
-        } else {
 
-            historyList.addLast(task);
         }
+
+        if (size > 9) {
+            removeFirst();
+        }
+        addLast(task);
+
     }
 
     @Override
-    public HistoryLinkedList<Task> getHistory() {
-        return historyList;
+    public ArrayList<Task> getHistory() {
+        return getTasks();
     }
 
-    static class HistoryLinkedList<Task> {
-        public Node<Task> start;
-        public Node<Task> end;
-        int size = 0;
-
-        public void addLast(Task task) {
-            final Node<Task> oldEnd = end;
-            Node<Task> thisNode = new Node<>(oldEnd, task, null);
-            end = thisNode;
-            if (oldEnd == null)
-                start = thisNode;
-            else
-                oldEnd.next = thisNode;
-            size++;
-        }
-
-        public int getSize(){
-            return size;
-        }
-
-        public ArrayList<Task> getTasks() {
-            ArrayList<Task> taskList = new ArrayList<>();
-            Node<Task> currHead = start;
-            if (currHead == null) throw new NoSuchElementException();
-            while (currHead != null) {
-                taskList.add(currHead.task);
-                currHead = currHead.next;
-            }
-            return taskList;
-        }
-        public void removeNode(Node<Task> node) {
-            final Node<Task> prevNode = node.prev;
-            final Node<Task> nextNode = node.next;
-            prevNode.next = node.next;
-            nextNode.prev = node.prev;
-        }
-
-        public void removeFirst() {
-            final Node<Task> nextNode = start.next;
-            nextNode.prev = null;
-            start = nextNode;
-        }
+    public void addLast(Task task) {
+        final Node<Task> oldEnd = end;
+        Node<Task> thisNode = new Node<>(oldEnd, task, null);
+        end = thisNode;
+        if (oldEnd == null)
+            start = thisNode;
+        else
+            oldEnd.next = thisNode;
+        historyListIdHolder.put(task.getId(), thisNode);
+        size++;
 
     }
+
+    public ArrayList<Task> getTasks() {
+        ArrayList<Task> taskList = new ArrayList<>();
+        Node<Task> currHead = start;
+        if (currHead == null) throw new NoSuchElementException();
+        while (currHead != null) {
+            taskList.add(currHead.task);
+            currHead = currHead.next;
+        }
+        return taskList;
+    }
+
+    public void removeNode(Node<Task> node) {
+        final Node<Task> prevNode = node.prev;
+        final Node<Task> nextNode = node.next;
+        if (prevNode != null ) prevNode.next = node.next;
+        if (start.task.equals(node.task)) start = nextNode;
+        if (nextNode != null) nextNode.prev = node.prev;
+        if (end.task.equals(node.task)) end = prevNode;
+        historyListIdHolder.remove(node.task.getId());
+        size = size - 1;
+    }
+
+    public void removeFirst() {
+        historyListIdHolder.remove(start.task.getId());
+         final Node<Task> nextNode = start.next;
+         if (nextNode == null) throw new NoSuchElementException();
+         nextNode.prev = null;
+         start = nextNode;
+         size = size -1;
+    }
+
 }
