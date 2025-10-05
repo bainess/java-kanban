dda966cdcd681257329282685d6294f48138b9de
Inessa Babenko <iness.tt.eln@gmail.com>
2025-10-05 16:20:40 +0700

    feat: add HistoryLinkedList

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -1,22 +1,69 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    List<Task> historyList = new ArrayList<>();
+    HistoryLinkedList<Task> historyList = new HistoryLinkedList<>();
+    Map<Integer, Task> historyListIdHolder= new HashMap<>();
 
     @Override
     public void addToHistoryList(Task task) {
-        if (historyList.size() > 10) {
+        if (historyList.getSize() > 10) {
+            historyListIdHolder.remove(task.getId());
             historyList.removeFirst();
-            historyList.add(task);
+            historyList.addLast(task);
+            historyListIdHolder.put(task.getId(), task);
         } else {
 
-            historyList.add(task);
+            historyList.addLast(task);
         }
     }
 
     @Override
-    public List<Task> getHistory() {
+    public HistoryLinkedList<Task> getHistory() {
         return historyList;
+    }
+
+    static class HistoryLinkedList<Task> {
+        public Node<Task> start;
+        public Node<Task> end;
+        int size = 0;
+
+        public void addLast(Task task) {
+            final Node<Task> oldEnd = end;
+            Node<Task> thisNode = new Node<>(oldEnd, task, null);
+            end = thisNode;
+            if (oldEnd == null)
+                start = thisNode;
+            else
+                oldEnd.next = thisNode;
+            size++;
+        }
+
+        public int getSize(){
+            return size;
+        }
+
+        public ArrayList<Task> getTasks() {
+            ArrayList<Task> taskList = new ArrayList<>();
+            Node<Task> currHead = start;
+            if (currHead == null) throw new NoSuchElementException();
+            while (currHead != null) {
+                taskList.add(currHead.task);
+                currHead = currHead.next;
+            }
+            return taskList;
+        }
+        public void removeNode(Node<Task> node) {
+            final Node<Task> prevNode = node.prev;
+            final Node<Task> nextNode = node.next;
+            prevNode.next = node.next;
+            nextNode.prev = node.prev;
+        }
+
+        public void removeFirst() {
+            final Node<Task> nextNode = start.next;
+            nextNode.prev = null;
+            start = nextNode;
+        }
+
     }
 }
